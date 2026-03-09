9aff60e629c9946e21d506c47d23e297814ada1f
DimaG <gulenkov.dmitriy@mail.com>
2026-03-09 14:31:24 +0300

    latest

--- app/components/layouts/ElHeader.vue
+++ app/components/layouts/ElHeader.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="header">
+    <header class="header py-[20px]">
         <div class="wrapper flex">
-            <nav class="nav flex gap-[90px] w-full items-center">
-                <ul class="nav__list flex gap-[50px] uppercase">
+            <nav class="nav flex gap-[60px] w-full items-center justify-around">
+                <ul class="nav__list flex gap-[45px] uppercase">
                     <li v-for="item in menuItems1" :key="item.id" class="nav__item">
                         <a :href="item.link" class="nav__link flex items-center gap-4">
                             <SvgIcon v-if="item.svgBool" :name="item.svgIcon" w="24px" h="24px" color="var(--red)"/>
@@ -11,9 +11,9 @@
                     </li>
                 </ul>
                 <a class="logo" href="index">
-                    <SvgIcon name="logo" w="108px" h="115px"/>
+                    <SvgIcon name="logo" w="189px" h="155.26px"/>
                 </a>
-                <ul class="nav__list flex gap-[50px] uppercase">
+                <ul class="nav__list flex gap-[45px] uppercase">
                     <li v-for="item in menuItems2" :key="item.id" class="nav__item">
                         <a :href="item.link" class="nav__link">
                             {{ item.name }}
@@ -33,9 +33,11 @@ import SvgIcon from '../UI/SvgIcon.vue';
             return {
                 menuItems1: [
                     { id: 1, name: 'Меню', link: 'menu' },
-                    { id: 2, name: 'Новости и события', link: 'news' },
-                    { id: 3, name: 'Акции и скидки', link: '#' },
-                    { id: 4, name: 'Забронировать', svgBool: true, svgIcon:'notify', link: '#' },
+                    { id: 2, name: 'Новости', link: 'news' },
+                    { id: 3, name: 'События', link: '#' },
+                    { id: 4, name: 'Спец. предложения', link:'#'},
+                    { id: 5, name: 'Банкеты', svgBool: true, svgIcon:'notify', link: '#' },
+                    
                 ],
                 menuItems2: [
                     {id: 1, name: 'Программа лояльности', link: 'loyalty'},
@@ -53,7 +55,7 @@ import SvgIcon from '../UI/SvgIcon.vue';
     height: 155px;
     &__list{
         font-size: 0.9375rem;
-        width: 654px;
+        width: 646px;
         text-align: end;
     }
     &__item{
